295e73231fc655cc477b82f64f2602d426e8126d
MaruCuello <85590314+marucuelloo@users.noreply.github.com>
2023-01-09 19:16:54 -0300

    tarea 1

---
+++ index.js
@@ -0,0 +1,12 @@
+//Este es el mensaje de bienvenida 
+console.log("Esta es la puerta de entrada al proyecto")
+
+//esta es la parte intermedia del proyecto
+//chequeos necesarios
+//vamos al saludo 
+
+/*este comentario sirve para cuando hay mas de una linea de código 
+y todo lo que esta dentro de esot, va estar comentado*/
+
+//este va ser el mensaje que se envia para despedir
+console.log("adios")

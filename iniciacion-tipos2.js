cb3aefca53d56f20fa9ea6b76af3d5d9c1e1a8cb
MaruCuello <85590314+marucuelloo@users.noreply.github.com>
2023-01-12 21:17:20 -0300

    video 2

---
+++ iniciacion-tipos2.js
@@ -0,0 +1,31 @@
+// Tipos primitivos
+
+// number
+4;
+0;
+
+// string
+"Hola mundo";
+'Hola mundo';
+`Hola mundo`;
+
+// booleanos
+true;
+false;
+
+// nulo y undefined
+//vacio
+null;
+//no esta definido
+undefined;
+// null, undefined, false, 0 ==> Todas son Falsy
+//null no es lo mismo que undefined
+console.log(null===undefined )
+
+if (true) {
+    console.log("cumple")
+} else {
+    console.log("no cumple")
+}
+
+

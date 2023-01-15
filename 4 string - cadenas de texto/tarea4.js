59c2e17619c51018751dbbaca3484457a9fa9e85
MaruCuello <85590314+marucuelloo@users.noreply.github.com>
2023-01-15 20:45:59 -0300

    eliminar todos los espacios de estudiante

--- 4 string - cadenas de texto/tarea4.js
+++ 4 string - cadenas de texto/tarea4.js
@@ -18,5 +18,7 @@ console.log(primerletra=estudiante.charAt(10));
 console.log(ultimaletra=estudiante.charAt(25));
 // - Una cadena de texto que elimine los espacios de la variable "estudiante"
 console.log(estudiante.trim().length); 
+
+console.log(estudiante.split(' ').join('').length);
 // - Una variable booleana que diga si el Nombre está contenido en la variable "estudiante"
 console.log(estudiante.includes(`${nombre}`)); 

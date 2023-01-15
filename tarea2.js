7cc70f586f455e72bc78050709b26dfc0b625a96
MaruCuello <85590314+marucuelloo@users.noreply.github.com>
2023-01-15 12:19:44 -0300

    tarea 2

---
+++ tarea2.js
@@ -0,0 +1,17 @@
+
+let nombre="Marianela";
+let edad=34;
+let dev=false;
+let fecnac= new Date(1988, 6, 02)
+
+
+const libroFav = {
+    titulo: "el mensaje de los sabios",
+    autor: "brian weiss",
+    fecha: new Date(1950, 7, 12),
+    url:"https://ia600901.us.archive.org/0/items/librosEDS/mensajessabios.pdf"     
+}
+
+const listaMaru = [nombre,edad,dev,fecnac, libroFav]
+
+console.log(listaMaru)

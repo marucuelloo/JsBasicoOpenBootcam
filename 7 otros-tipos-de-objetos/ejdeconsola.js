dc2ae813f0fc17cf04885b497317ebf42c428a09
MaruCuello <85590314+marucuelloo@users.noreply.github.com>
2023-01-26 18:07:15 -0300

    ej de consola ej 4 de objetos

---
+++ 7 otros-tipos-de-objetos/ejdeconsola.js
@@ -0,0 +1,22 @@
+// - Abre una nueva ventana "about:blank" en Google Chrome
+
+// - Abre la consola de desarrollador de Google Chrome (F12)
+
+// - Pregunta al usuario cuál es su edad y almacénala en una variable llamada edad
+
+//esto lo realiza en consola
+//le preg al usuario en consola
+prompt("Cual es tu edad?")
+//el valor que ingresa lo devuelve en string
+'34'
+//lo debo guardar en una var
+const edad = prompt("cual es tu edad?")
+undefined
+//llamo la var
+edad
+'34'
+//todos los valores que ingresa el usuario los devuelve en string, lo parseo a numero
+const num_edad=Number(edad)
+undefined
+num_edad
+34

2c6460ea8d989e5897fea1744be4c55df32fdd70
maru trabajo <marucuelloo@gmail.com>
2023-03-22 16:51:23 -0300

    eslint y ejercicio

---
+++ 13-eslint/eslint-npm/index.js
@@ -0,0 +1,21 @@
+//este es el archvio que vamos a usar
+
+const nombre= "Maru"
+//en esta linea quiero q desactives la regla de las comillas dobles. quiero comillas simples 
+//eliminar eslint y se pone con es tipo de comillas 
+/*eslint-disable*/
+const persona2='maria'
+/*eslint-enable*/
+const nuevoString= 'esto es un nuevo string ' //eslint-disable-line
+const nuevoString2= 'esto es un nuevo string ' //eslint-disable-line quotes 
+//(me marca el error del ; y no el de las comillas )
+
+
+/*eslint-disable-next-line */
+const nuevoString3= 'esto es un nuevo string '
+
+console.log("hola")
+
+const nuevoString4= "esto es un nuevo string "
+
+
---
+++ 13-eslint/index.js
@@ -0,0 +1,15 @@
+const nombre = "Maru"
+var nombre2 = "Nani";
+
+let nombre3 = 'Facu';
+
+var objeto={
+    nombre:"circulo",
+    radio:2,
+    nombre:"rectangulo"
+
+}
+
+
+// no me funciona, xq no me corrige las condicones que puse en el archivo eslintrs
+
